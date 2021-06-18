02a531d195bcdc1c5587350b884618a8200857bf
Arijit Ganguly <65554220+Arijit-datascience@users.noreply.github.com>
2021-06-18 15:56:50 +0530

    Update model.py

--- models/model.py
+++ models/model.py
@@ -11,11 +11,11 @@ class Net(nn.Module):
 
         # CONVOLUTION BLOCK 1
         self.convblock1 = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), bias=False),
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(32), # output_size = 32, RF = 3
 
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), bias=False),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(64),
 
@@ -24,66 +24,61 @@ class Net(nn.Module):
         # TRANSITION BLOCK 1
         self.transblock1 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(1, 1), bias=False), # output_size = 34, RF = 5
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=1, bias=False),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=1, bias=False),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=1, bias=False),
-        ) # output_size = 18, RF = 6
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+        ) # output_size = 26, RF = 11
 
         # CONVOLUTION BLOCK 2
         self.convblock2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), bias=False),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5, 5), bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value), # output_size = 18, RF = 10
-
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.Dropout(dropout_value)
-        ) # output_size = 18, RF = 14
+        ) # output_size = 22, RF = 15
 
         # TRANSITION BLOCK 2
         self.transblock2 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=(1, 1), bias=False), # output_size = 20, RF = 14
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), dilation=1, bias=False),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), dilation=1, bias=False),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), dilation=1, bias=False),
-        ) # output_size = 11, RF = 16
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(1, 1), bias=False), # output_size = 20, RF = 14
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+        ) # output_size = 16, RF = 21
 
         # CONVOLUTION BLOCK 3 -> DWS and Dialted Conv
         self.convblock3 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), padding=1, groups=64, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), groups=32, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(32),
+            nn.Dropout(dropout_value), # output_size = 11, 24
+
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), bias=False),
             nn.ReLU(),
             nn.BatchNorm2d(64),
             nn.Dropout(dropout_value), # output_size = 11, 24
-
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(1, 1), padding=1, bias=False),
-            nn.ReLU(),
-            nn.BatchNorm2d(128),
-            nn.Dropout(dropout_value), # output_size = 11, 24
-        ) # output_size = 9, RF = 40
+        ) # output_size = 20, RF = 23
 
         # TRANSITION BLOCK 3
         self.transblock3 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=(1, 1), padding=1, bias=False), # output_size = 11, RF = 40
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), dilation=1, bias=False),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), dilation=1, bias=False),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), dilation=1, bias=False),
-        )
+            nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(1, 1), bias=False), # output_size = 11, RF = 40
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), dilation=2, bias=False),
+        )# output_size = 14, RF = 27
 
         # CONVOLUTION BLOCK 4
         self.convblock4 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), bias=False),
             nn.ReLU(),
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(64),
             nn.Dropout(dropout_value), # output_size = 6, RF = 60
-        ) # output_size = 6, RF = 76
+        ) # output_size = 12, RF = 29
 
         # OUTPUT BLOCK
         self.opblock = nn.Sequential(
             nn.AvgPool2d(kernel_size=7), # output_size = 1
 
-            nn.Conv2d(in_channels=128, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.Conv2d(in_channels=64, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
             # nn.BatchNorm2d(10),
             # nn.ReLU(),
             # nn.Dropout(dropout_value)
