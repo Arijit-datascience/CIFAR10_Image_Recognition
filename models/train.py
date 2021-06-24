bf8be30bd8ef957e3204c4be49fbd6134ae03318
Arijit Ganguly <65554220+Arijit-datascience@users.noreply.github.com>
2021-06-24 17:43:28 +0530

    Structural changes

--- models/train.py
+++ models/train.py
@@ -3,41 +3,34 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-# from torch.optim.lr_scheduler import StepLR
-
-def train(model, device, train_loader, optimizer, epoch, train_losses, train_acc):
-    
-    # Train network
+def train(model, device, train_loader, optimizer, scheduler, epoch, l1_factor):
     model.train()
+    epoch_loss = 0
     correct = 0
-    processed = 0
-    l1_factor = 0
-    
+
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
-
-        # Calculate loss
         loss = F.nll_loss(output, target)
-        L1_loss = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
-        reg_loss = 0 
-        for param in model.parameters():
+
+        if L1:
+          L1_loss = nn.L1Loss(size_average=None, reduce=None, reduction='mean')
+          reg_loss = 0 
+          for param in model.parameters():
             zero_vector = torch.rand_like(param) * 0
             reg_loss += L1_loss(param,zero_vector)
-        loss += l1_factor * reg_loss
+          loss += l1_factor * reg_loss
 
-        train_losses.append(loss)
-
-        # Backpropagation
+        epoch_loss += loss.item()
         loss.backward()
         optimizer.step()
-
+        scheduler.step()
         pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
         correct += pred.eq(target.view_as(pred)).sum().item()
-        processed += len(data)
 
-        train_acc.append(100*correct/processed)
-
-    print('\nTrain set: Average loss: {:.4f}, Train Accuracy: {}/{} ({:.2f}%)\n'.format(
-    loss, correct, len(train_loader.dataset),100. * correct / len(train_loader.dataset)))
+    print(f'Train set: Average loss: {loss.item():.4f}, Accuracy: {100. * correct/len(train_loader.dataset):.2f}')
+    train_loss = epoch_loss / len(train_loader)
+    train_acc=100.*correct/len(train_loader.dataset)
+    return train_loss, train_acc
+    
